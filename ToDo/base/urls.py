12b56fa27b5480e8cab2c7029774d905c3593eff
SL02 <SL02@nabilbank.com>
2023-10-08 21:32:22 +0545

    Inital commit

---
+++ ToDo/base/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('home/', home, name = 'home'),
+    path('create/', create, name = 'create'),
+    path('edit/<int:pk>/', edit, name = 'edit'),
+    path('delete/<int:pk>/', delete, name = 'delete'),
+]
---
+++ ToDo/base/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import ToDo
+
+# Create your views here.
+
+# def home(request):
+#     return HttpResponse('This is a Home Page!')
+
+def home(request):
+    todo_objects = ToDo.objects.all()
+    content = {'todos' : todo_objects}
+    return render(request,'index.html', context = content)
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get("description")
+        status = request.POST.get("status")
+        # print(name, description, status)
+        ToDo.objects.create(name = name, description = description, status = status)
+        return redirect('home')
+    content = {'mode' : 'create'}
+    return render(request, 'create.html', context = content)
+
+def edit(request, pk):
+    todo_object = ToDo.objects.get(id = pk)
+    content = {'mode' : 'edit', 'todo' : todo_object}
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        todo_object.name = name
+        todo_object.description = description
+        todo_object.status = status
+        todo_object.save()
+        return redirect('home')
+    return render(request, 'create.html', context = content)
+
+
+def delete(request, pk ):
+    todo_object = ToDo.objects.get(id = pk )
+    todo_object.delete()
+    return redirect('home')
