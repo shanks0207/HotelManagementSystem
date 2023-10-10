c0129b644bd09c65198e13d7618b370755b66a51
SL02 <SL02@nabilbank.com>
2023-10-10 21:14:29 +0545

    First commit, Hotel Management System
    second Django project created & all necessary app created.

--- ToDo/base/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from .views import *
-urlpatterns = [
-    path('home/', home, name = 'home'),
-    path('create/', create, name = 'create'),
-    path('edit/<int:pk>/', edit, name = 'edit'),
-    path('delete/<int:pk>/', delete, name = 'delete'),
-]
